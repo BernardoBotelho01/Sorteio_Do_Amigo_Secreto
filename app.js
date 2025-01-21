07faffe8f9e9a7e4c9173381eccdb0046f258d89
Bernardo_Botelho <82350234+BernardoBotelho01@users.noreply.github.com>
2025-01-20 22:53:05 -0300

    Primeiras linhas de codigo

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let inputAmigo = document.querySelector('#amigo');
+    let nomeAmigo = inputAmigo.value;
+
+    if (nomeAmigo === '') {
+        prompt('Por favor, insira um nome válido.');
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+
+    atualizarLista();
+
+    inputAmigo.value = '';
+}
+
+function atualizarLista() {
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = ''; 
+
+    let i = 0; 
+    while (i < amigos.length) {
+        let itemLista = document.createElement('li');
+        itemLista.textContent = amigos[i]; 
+        listaAmigos.appendChild(itemLista); 
+        i++; 
+    }
+}
